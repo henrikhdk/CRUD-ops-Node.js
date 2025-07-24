4a372ab277527f0925430fb6ad6d004ac272e10c
Henrik D'oark <henrikdoark@gmail.com>
2025-07-24 15:47:05 -0400

    Create an endpoint for sorting users by date of birth

--- routes/users.js
+++ routes/users.js
@@ -37,6 +37,24 @@ router.get("/lastName/:lastName", (req, res) => {
     res.send(filtered_lastname);
 });
 
+// Function to convert a date string in the format "dd-mm-yyyy" to a Date object
+function getDateFromString(strDate) {
+    let [dd, mm, yyyy] = strDate.split('-');
+    return new Date(yyyy + "/" + mm + "/" + dd);
+}
+
+// Define a route handler for GET requests to the "/sort" endpoint
+router.get("/sort", (req, res) => {
+    // Sort the users array by DOB in ascending order
+    let sorted_users = users.sort(function(a, b) {
+        let d1 = getDateFromString(a.DOB);
+        let d2 = getDateFromString(b.DOB);
+        return d1 - d2;
+    });
+    // Send the sorted_users array as the response to the client
+    res.send(sorted_users);
+});
+
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
     // Extract the email parameter from the request URL
